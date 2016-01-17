69821485cc344c3626808b729c0d972e8bac566d
Mehran Kholdi <semekh.dev@gmail.com>
2016-01-17 21:43:09 +0330

    Enforce `state='file'` in `copy` module
    This was causing wrong behaviour when `prev_state` was `hard`-link,
    since the `file` module tried to apply the same `state` on the new
    file, causing unexpected errors.
    
    Particularly, both `overlay` and `devicemapper` storage drivers in
    docker use hardlinks to share files between layers. This causes
    most ansible playbooks to fail when working with files from layers
    below.

--- lib/ansible/plugins/action/copy.py
+++ lib/ansible/plugins/action/copy.py
@@ -268,7 +268,8 @@ class ActionModule(ActionBase):
                     dict(
                         src=source_rel,
                         dest=dest,
-                        original_basename=source_rel
+                        original_basename=source_rel,
+                        state='file'
                     )
                 )
 
